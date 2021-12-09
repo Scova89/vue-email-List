7aa6e92d1190c33170d4d81c97cb8ad9b32171cb
Francesco <scovascova@gmail.com>
2021-12-09 19:19:11 +0100

    script axios

---
+++ script.js
@@ -0,0 +1,25 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+
+    },
+    created(){
+        // Make a request for a user with a given ID
+        axios.get('/user?ID=12345')
+        .then(function (response) {
+        // handle success
+        console.log(response);
+        })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+        })
+        .then(function () {
+        // always executed
+        });
+    },
+
+});
